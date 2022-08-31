fc68d10649bb62529b17ae28b5a375815d41f606
Archie McKay <archiemckay2000@gmail.com>
2022-08-31 14:21:52 +1000

    First commit to ROSTutorials

---
+++ catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/archiem/Development/COMP3760_Labs/ROSTutorials/catkin_ws/src/beginner_tutorials
+# Build directory: /home/archiem/Development/COMP3760_Labs/ROSTutorials/catkin_ws/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/archiem/Development/COMP3760_Labs/ROSTutorials/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/archiem/Development/COMP3760_Labs/ROSTutorials/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/archiem/Development/COMP3760_Labs/ROSTutorials/catkin_ws/devel;/opt/ros/noetic' != '' else []
